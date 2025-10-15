3423671db11e1a4e1c5d242c7cf15ab3b0de006e
ThDilos <2273656532co@gmail.com>
2025-10-15 15:46:33 +0800

    Implemented Pause menu into Level0 scenes. BGM source is not added currently so it's skipped

--- 3D-Test/Assets/Scripts/UI/PauseMenu.cs
+++ 3D-Test/Assets/Scripts/UI/PauseMenu.cs
@@ -1,16 +1,67 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    public Button quitButton;
+    [SerializeField] private GameObject settingPanel;
+    [SerializeField] private Slider sfxSlider;
+    [SerializeField] private AudioSource sfxSource;
+
+    // Runtime Vars
+    private GameController gc;
+    private InputActionAsset inputActions;
+    private InputAction togglePauseMenu;
+    private bool gamePaused = false;
+
+    private void Start()
+    {
+        gc = GameController.Instance;
+        inputActions = gc.inputActions;
+        InputActionMap map = inputActions.FindActionMap("Utils");
+        togglePauseMenu = map.FindAction("Pause");
+        settingPanel.SetActive(gamePaused);
+
+        sfxSource.volume = sfxSlider.value;
+    }
+
     void Update()
     {
-        quitButton.onClick.AddListener(Quitgame);
+        if (togglePauseMenu.WasPerformedThisFrame())
+        {
+            TogglePauseMenu();
+        }
 
+        sfxSlider.onValueChanged.AddListener(delegate { sfxSource.volume = sfxSlider.value; });
     }
+
     public void Quitgame()
     {
+        Debug.Log("Quitted Game");
         Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        // Reload Current Scene
+        gc.ResumeGame();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void TogglePauseMenu()
+    {
+        gamePaused = !gamePaused;
+        settingPanel.SetActive(gamePaused);
+
+        if (gamePaused)
+        {
+            gc.PauseGame();
+        }
+        else
+        {
+            gc.ResumeGame();
+        }
     }
 }
